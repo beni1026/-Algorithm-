96b6cc125c85bc59078ad06911b1501a1ef5a0d4
EUNA <beni1026@naver.com>
2021-12-20 22:05:29 +0900

    1120 문자열

--- 1120 문자열/소스.cpp
+++ 1120 문자열/소스.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 #include <algorithm> 
+#include <string>
 using namespace std;
-
-int n, ans;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(nullptr);
 
-    cin >> n;
-    while (1) {
-        if (n == 0) break;
-        if (n % 5 == 0) {
-            ans += n / 5;
-            break;
+    string s1, s2;
+    cin >> s1 >>  s2;
+    int cnt = 0;
+    int mincnt = 1e9;
+    if (s1.length() == s2.length()) {
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] != s2[i]) cnt++;
         }
-        if (n < 3) {
-            ans = -1;
-            break;
+        cout << cnt;
+    }
+    else {
+        int j = 0;
+        int sub = s2.length() - s1.length();
+        while (j <= sub) {
+            cnt = 0;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1[i] != s2[i+j]) cnt++;
+            }
+            mincnt = min(cnt, mincnt);
+            j++;
         }
-        n -= 3;
-        ans++;
+        cout << mincnt;
     }
 
     
-    
 
-    cout << ans;
 
 }
